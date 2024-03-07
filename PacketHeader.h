6274c28e8cca2d1e86fe46ca439c46c3d4abcd32
AmanTrechsel <112391335+AmanTrechsel@users.noreply.github.com>
2024-03-07 13:26:46 +0100

    Update PacketHeader.h

--- PacketHeader.h
+++ PacketHeader.h
@@ -11,7 +11,7 @@ struct PacketHeader
   uint8_t m_gameMajorVersion; // Game major version - "X.00"
   uint8_t m_gameMinorVersion; // Game minor version - "1.XX"
   uint8_t m_packetVersion; // Version of this packet type, all start from 1
-  uint8_t m_packetId; // Identifier for the packet type, see below
+  int8_t m_packetId; // Identifier for the packet type, see below
   uint64_t m_sessionUID; // Unique identifier for the session
   float m_sessionTime; // Session timestamp
   uint32_t m_frameIdentifier; // Identifier for the frame the data was retrieved on
