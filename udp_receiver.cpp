5240a54b4f035b164de9f9398a1badbc0f81b1b1
AmanTrechsel <112391335+AmanTrechsel@users.noreply.github.com>
2024-03-07 12:39:23 +0100

    Update udp_receiver.cpp

--- udp_receiver.cpp
+++ udp_receiver.cpp
@@ -60,14 +60,13 @@ int main() {
     while (true) {
         int n = recvfrom(sockfd, (char *)buffer, sizeof(buffer), 0, (struct sockaddr *)&cliaddr, &len);
         buffer[n] = '\0'; // Voeg een nulbyte toe aan het einde van de ontvangen gegevens
-        h_packet.get(buffer);
-
-        //std::cout << "Ontvangen bericht: " << h_packet.m_sessionTime << std::endl;
 
         unsigned long offset = h_packet.get(buffer); // Haal de offset op van de PacketHeader
+
+        //std::cout << "Ontvangen bericht: " << h_packet.m_sessionTime << std::endl;
         switch (h_packet.m_packetId) {
             case PACKET_ID_MOTION:  // 0 - Motion
-                p_motion.get(buffer); // Geef de offset mee aan CarMotionData
+                p_motion.get(buffer, offset); // Geef de offset mee aan CarMotionData
                 std::cout << "Velocity: " << p_motion.m_worldVelocityX << ","  << p_motion.m_worldVelocityY << ","  << p_motion.m_worldVelocityZ << std::endl;
                 break;
             // Voeg andere gevallen voor andere pakkettypen toe indien nodig
